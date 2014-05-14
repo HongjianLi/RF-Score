e0729eafde511a53d23388593c98f29073bdd50d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-14 15:57:43 +0800

    Added trns

--- cyscore/corplot.R
+++ cyscore/corplot.R
@@ -4,25 +4,20 @@ xs=c(4,46)
 ms=c("mlr","rf")
 nm=length(ms) # Number of models.
 tsts=c(195,201,592)
-ns=length(tsts) # Number of benchmarks.
+ns=length(tsts) # Number of test sets.
+trns=c(247,247,247,1105,2696,2367)
+nr=length(trns) # Number of training sets.
 pdf("cor.pdf",width=5*ns,height=5*nm)
-layout(matrix(1:(nm*ns),2,ns,byrow=T))
+layout(matrix(1:nr,nm,ns,byrow=T))
 par(cex.lab=1.4,cex.axis=1.4,cex.main=1.4)
 for (mi in 1:2)
 {
 	x=xs[mi]
 	m=ms[mi]
-	for (tst in tsts)
+	for (ti in 1:ns)
 	{
-		if (mi==1) {
-			trn=247
-		} else if (tst==195) {
-			trn=1105
-		} else if (tst==201) {
-			trn=2696
-		} else {
-			trn=2367
-		}
+		tst=tsts[ti]
+		trn=trns[(mi-1)*3+ti]
 		d=read.csv(sprintf("x%d/%s/trn-%s-tst-%s-iyp.csv",x,m,trn,tst))
 		n=nrow(d) # Number of samples.
 		rmse=sqrt(sum((d["predicted"] - d["pbindaff"])^2)/n)
