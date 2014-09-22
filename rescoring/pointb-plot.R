c7af73e97188445b4d436727c65f9b5aab408b49
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-09-22 09:47:04 +0800

    Updated pointb-plot.R

--- rescoring/pointb-plot.R
+++ rescoring/pointb-plot.R
@@ -2,8 +2,9 @@
 d=as.matrix(read.csv('pointb.csv'))
 c="sdev"
 xylim=c(min(d[,c]),max(d[,c]))
-tiff("pointb.tiff", compression="lzw")
+#tiff("pointb.tiff", compression="lzw")
+pdf("pointb.pdf")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(d[1:51,c],d[52:102,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::VinaElem",main="Scatter plot of SD of models 2 and 4 trained on PDBbind v2012\nrefined set (N=2897) and tested on 10 5-fold partitions of\nPDBbind v2013\\v2012 refined set (N=382)")
+plot(d[2:51,c],d[53:102,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::VinaElem",main="Scatter plot of SD of models 2 and 4 on 50 test sets")
 abline(0, 1)
 dev.off()
