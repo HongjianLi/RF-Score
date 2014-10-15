b2e6c43d332d20d9f38cb9dd5c6d9cb5b318a711
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-10-15 13:16:36 +0800

    Replaced model 4 by model 3

--- rescoring/pointb-plot.R
+++ rescoring/pointb-plot.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
-d=as.matrix(read.csv('pointb.csv'))
+d=read.csv('pointb.csv')
 c="sdev"
 xylim=c(min(d[,c]),max(d[,c]))
 #tiff("pointb.tiff", compression="lzw")
 pdf("pointb.pdf")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(d[2:51,c],d[53:102,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::VinaElem",main="Scatter plot of SD of models 2 and 4 on 50 test sets")
+plot(d[d$model==2,c],d[d$model==3,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::Vina",main="Scatter plot of SD of models 2 and 3 on 50 test sets")
 abline(0, 1)
 dev.off()
