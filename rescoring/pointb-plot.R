cc06c89be2755176417cceb333ec3fe77c0da869
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-11-09 09:44:04 +0800

    Added pch=20

--- rescoring/pointb-plot.R
+++ rescoring/pointb-plot.R
@@ -2,9 +2,9 @@
 d=read.csv('pointb.csv')
 c="sdev"
 xylim=c(min(d[,c]),max(d[,c]))
-#tiff("pointb.tiff", compression="lzw")
-pdf("pointb.pdf")
+tiff("pointb.tiff", compression="lzw")
+#pdf("pointb.pdf")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(d[d$model==2,c],d[d$model==3,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::Vina",main="Scatter plot of SD of models 2 and 3 on 50 test sets")
+plot(d[d$model==2,c],d[d$model==3,c],xlim=xylim,ylim=xylim,xlab="MLR::Vina",ylab="RF::Vina",main="SD for both SFs on 50 test sets",pch=20)
 abline(0, 1)
 dev.off()
